16e9b0f98deffaecb2683811f225dee3f1665367
Angga Adhya Pratama <sumedangtempe@gmail.com>
2024-08-15 00:00:23 +0700

    first commit

--- js/script.js
+++ js/script.js
@@ -12,15 +12,20 @@ function calculateBMI() {
         let description;
         if (bmi < 18.5) {
             description = "Berat badan anda kurang";
+            description2 = "Hubungi dokter lebih lanjut mengenai pola makan dan gizi yang baik untuk meningkatkan kesehatan.";
         } else if (bmi >= 18.5 && bmi <= 22.9) {
             description = "Berat badan anda Normal";
+            description2 = "Tetap pertahankan berat badan Anda dan jaga berat badan Anda dengan mengatur keseimbangan antara pola makan dan aktivitas fisik Anda.";
         } else if (bmi >= 23 && bmi <= 24.9) {
             description = "Berat badan anda berlebih";
+            description2 = "Cara terbaik untuk menurunkan berat badan adalah dengan mengatur kalor makanan yang dikonsumsi dan berolahraga.";
         } else {
             description = "Anda obesitas";
+            description2 = "Usahakan untuk menurunkan berat badan dan menerapkan pola hidup sehat dengan menjaga makan dan aktivitas fisik.";
         }
 
-        document.getElementById("bmiDescription").innerText = `Kategori: ${description}`;
+        document.getElementById("bmiDescription").innerText = `${description}`;
+        document.getElementById("bmiDescription2").innerText = `${description2}`;
     } else {
         alert("Please fill in all fields");
     }
