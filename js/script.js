ee43b93da65ea69dab8a6281d7cc104b93ffb46e
Angga Adhya Pratama <sumedangtempe@gmail.com>
2024-08-15 00:49:33 +0700

    commit 3

--- js/script.js
+++ js/script.js
@@ -24,6 +24,7 @@ function calculateBMI() {
             description2 = "Usahakan untuk menurunkan berat badan dan menerapkan pola hidup sehat dengan menjaga makan dan aktivitas fisik.";
         }
 
+        //Hasil yang tertampilkan
         document.getElementById("bmi-description").innerText = `${description}`;
         document.getElementById("bmi-description2").innerText = `${description2}`;
     } else {
